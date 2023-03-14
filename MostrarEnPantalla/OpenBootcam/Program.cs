e4bf228e1fce7ffb5a0d92ab2afcddfb92f12efc
steven-molina <101531089+steven-molina@users.noreply.github.com>
2023-03-14 18:47:23 -0500

    Add files via upload

---
+++ MostrarEnPantalla/OpenBootcam/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBootcam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("ingrese su nombre");
+              nombre = Console.ReadLine();
+
+            Console.WriteLine($"su nombre es: { nombre}");
+
+
+        }
+    }
+}
