9a6094e1b42187c124fe75d4c7ee6dd6489947c1
steven-molina <101531089+steven-molina@users.noreply.github.com>
2023-03-14 18:52:54 -0500

    Add files via upload

---
+++ Hora/OpenBootcam/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBootcam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hora;
+
+            Console.WriteLine("¿que hora es?");
+            hora = Console.ReadLine();
+            Console.WriteLine($"La hora es: {hora}");
+            Console.Read();
+        }
+    }
+}
